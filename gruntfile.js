0b763594f7b613dddf3bc031287665ff534eb649
Trent Mentink <trentmentink@gmail.com>
2017-04-09 21:14:10 -0700

    added eol rule to eslint

--- gruntfile.js
+++ gruntfile.js
@@ -68,4 +68,3 @@ module.exports = function(grunt) {
   grunt.registerTask("default", ["eslint", "concat", "babel", "uglify", "clean"])
   grunt.registerTask("lint", ["eslint"])
 }
-
--- src/js/vendor/jquery-shim.js
+++ src/js/vendor/jquery-shim.js
@@ -181,4 +181,3 @@
   window.$ = $
 
 })(window)
-
