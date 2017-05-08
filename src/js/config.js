89c17ae1a1d617d92e06d81461040ffee90d19a7
Trent Mentink <trentmentink@gmail.com>
2017-05-07 21:39:49 -0700

    flattened config defaults

--- src/js/config.js
+++ src/js/config.js
@@ -12,36 +12,35 @@
 
   gmap.Config = {
 
-    Defaults: {
-      Label: {
-        fontSize: 14,
-        fontColor: "#000",
-        strokeColor: "#FFF",
-        strokeWeight: 1,
-        align: "center"
-      },
-      Map: {
-        zoom: 6,
-        center: { lat: 37.5, lng: -120 },
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        clickableIcons: false,
-        mapTypeControl: false,
-        streetViewControl: false
-      },
-      Marker: {
-
-      },
-      Polygon: {
-        strokeColor: "#000",
-        strokeOpacity: 0.8,
-        strokeWeight: 1,
-        fillColor: "#0275D8",
-        fillOpacity: 0.8
-      }
-    },
-    Delimeter: {
+    Delimiter: {
       LatLng: ",",
       LatLngPair: "|"
+    },
+    LabelOptions: {
+      fontSize: 14,
+      fontColor: "#000",
+      strokeColor: "#FFF",
+      strokeWeight: 1,
+      align: "center"
+    },
+    MapId: "gmap",
+    MapOptions: {
+      zoom: 6,
+      center: { lat: 37.5, lng: -120 },
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      clickableIcons: false,
+      mapTypeControl: false,
+      streetViewControl: false
+    },
+    MarkerOptions: {
+
+    },
+    PolygonOptions: {
+      strokeColor: "#000",
+      strokeOpacity: 0.8,
+      strokeWeight: 1,
+      fillColor: "#0275D8",
+      fillOpacity: 0.8
     }
 
   }
