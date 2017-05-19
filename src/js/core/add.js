d990b3b68fc711d31b443c123956895fd59bba21
Trent Mentink <trentmentink@gmail.com>
2017-05-19 11:24:42 -0700

    updated add method with error messages and parm conversions by type

--- src/js/core/add.js
+++ src/js/core/add.js
@@ -10,6 +10,44 @@
   // Constants
   // ----------------------------------------------------------------------
 
+  const Conversions = {
+    paths: function(parms) {
+      if ($.type(parms.paths) == "string") {
+        parms.paths = gmap.Util.toLatLngArray(parms.paths)
+      }
+    },
+    position: function(parms) {
+      if ($.type(parms.position) == "string") {
+        parms.position = gmap.Util.toLatLng(parms.position)
+      }
+    },
+    text: function(parms) {
+      parms.text = parms.text || parms.id
+    }
+  }
+
+  const ConvertableParms = {
+    Label: {
+      position: Conversions.position,
+      text: Conversions.text
+    },
+    Marker: {
+      position: Conversions.position
+    },
+    Polygon: {
+      paths: Conversions.paths
+    }
+  }
+
+  const ErrorMessages = {
+    IdExists: function(type, id) {
+      return "Error: A " + type + " with the id " + id + " already exists"
+    },
+    ParmIsRequired: function(parm) {
+      return "Error: " + parm + " is required"
+    }
+  }
+
   const RequiredParms = {
     Label: [
       "id", "position"
@@ -35,7 +73,7 @@
     }
 
     if ($.type(parms) == "object") {
-      if (_validParameters(map, type, parms)) {
+      if (_validateParms(map, type, parms)) {
         return _add(map, type, parms)
       }
     }
@@ -47,27 +85,9 @@
   // ----------------------------------------------------------------------
 
   function _add(map, type, parms) {
-    // TODO: make this intelligent on what parms are required
-    if ($.type(parms.path) == "string") {
-      parms.path = gmap.Util.toLatLngArray(parms.path)
-    }
+    _convertParms(type, parms)
 
-    if ($.type(parms.paths) == "string") {
-      parms.paths = gmap.Util.toLatLngArray(parms.paths)
-    }
-
-    if ($.type(parms.position) == "string") {
-      parms.position = gmap.Util.toLatLng(parms.position)
-    }
-
-
-    const defaults = map.Config[type + "Options"] || {}
-    const options = $.extend({}, defaults, parms.options)
-    options.map = map.Obj
-    options.paths = parms.paths || parms.path
-    options.position = parms.position
-    options.text = parms.text || parms.id
-
+    let options = _mergeDefaults(map, type, parms)
     map.Components[type][parms.id] = new gmap[type](parms.id, options)
     return map.Components[type][parms.id]
   }
@@ -78,7 +98,7 @@
     for (var i = 0, i_end = parmsArray.length; i < i_end; i++) {
       let parms = parmsArray[i]
 
-      if (_validParameters(map, type, parms)) {
+      if (_validateParms(map, type, parms)) {
         newCompArray[parms.id] = _add(map, type, parms)
       }
     }
@@ -86,16 +106,34 @@
     return newCompArray
   }
 
-  function _validParameters(map, type, parms) {
-    const required = RequiredParms[type]
-    for (var i = 0, i_end = required.length; i < i_end; i++) {
-      if (!parms[required[i]]) {
-        throw "Error: " + required[i] + " is required"
+  function _convertParms(type, parms) {
+    Object.keys(ConvertableParms[type]).forEach(function(key) {
+      ConvertableParms[type][key](parms)
+    })
+  }
+
+  function _mergeDefaults(map, type, parms) {
+    const defaults = map.Config[type + "Options"] || {}
+    const options = $.extend({}, defaults, parms)
+    options.map = map.Obj
+
+    // delete any parms that are only for gmaps
+    delete options.id
+
+    return options
+  }
+
+  function _validateParms(map, type, parms) {
+    for (var i = 0, i_end = RequiredParms[type].length; i < i_end; i++) {
+      let reqParm = RequiredParms[type][i]
+
+      if (!parms[reqParm]) {
+        throw ErrorMessages.ParmIsRequired(reqParm)
       }
     }
 
     if (map.Components[type][parms.id]) {
-      throw "Error: Id already exists"
+      throw ErrorMessages.IdExists(type, parms.id)
     }
 
     return true
