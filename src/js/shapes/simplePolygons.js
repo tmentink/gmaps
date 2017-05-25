a64e75a14a4333e416c969fa10d94f32905cb7e6
Trent Mentink <trentmentink@gmail.com>
2017-05-25 15:47:18 -0700

    removed global Shapes namespace and added methods to gmap prototype. added decagon. removed Util.convertShapeOptions.

--- src/js/shapes/simplePolygons.js
+++ src/js/shapes/simplePolygons.js
@@ -2,7 +2,7 @@
 // GMaps: simplePolygons.js
 // ------------------------------------------------------------------------
 
-!((Shapes, Util) => {
+!((gmap, Util) => {
   "use strict"
 
 
@@ -11,6 +11,7 @@
   // ----------------------------------------------------------------------
 
   const Shape = {
+    DECAGON:   "Decagon",
     HEXAGON:   "Hexagon",
     PENTAGON:  "Pentagon",
     RECTANGLE: "Rectangle",
@@ -19,6 +20,7 @@
   }
 
   const ShapeDegrees = {
+    Decagon:   [ 36, 72, 108, 144, 180, 216, 252, 288, 324, 360 ],
     Hexagon:   [ 30, 90, 150, 210, 270, 330 ],
     Pentagon:  [ 72, 144, 216, 288, 360 ],
     Rectangle: [ 60, 120, 240, 300 ],
@@ -31,24 +33,28 @@
   // Public Functions
   // ----------------------------------------------------------------------
 
-  Shapes.hexagon = function(parms) {
-    return _getShapePath(Shape.HEXAGON, parms)
+  gmap.prototype.decagon = function(parms) {
+    return _getShapePath(this, parms, Shape.DECAGON)
   }
 
-  Shapes.pentagon = function(parms) {
-    return _getShapePath(Shape.PENTAGON, parms)
+  gmap.prototype.hexagon = function(parms) {
+    return _getShapePath(this, parms, Shape.HEXAGON)
   }
 
-  Shapes.rectangle = function(parms) {
-    return _getShapePath(Shape.RECTANGLE, parms)
+  gmap.prototype.pentagon = function(parms) {
+    return _getShapePath(this, parms, Shape.PENTAGON)
   }
 
-  Shapes.square = function(parms) {
-    return _getShapePath(Shape.SQUARE, parms)
+  gmap.prototype.rectangle = function(parms) {
+    return _getShapePath(this, parms, Shape.RECTANGLE)
   }
 
-  Shapes.triangle = function(parms) {
-    return _getShapePath(Shape.TRIANGLE, parms)
+  gmap.prototype.square = function(parms) {
+    return _getShapePath(this, parms, Shape.SQUARE)
+  }
+
+  gmap.prototype.triangle = function(parms) {
+    return _getShapePath(this, parms, Shape.TRIANGLE)
   }
 
 
@@ -56,25 +62,23 @@
   // Private Functions
   // ----------------------------------------------------------------------
 
-  function _getShapePath(type, parms) {
-    if (parms.map) {
-      parms.center = parms.map.getCenter()
-      parms.size   = Util.getSizeFromZoom(parms.map.getZoom())
-    }
-    else {
-      parms = Util.convertShapeOptions(type, parms)
+  function _getShapePath(map, parms, type) {
+    parms        = $.isPlainObject(parms) ? parms : {}
+    parms.center = parms.center || map.getCenter()
+    parms.size   = parms.size || Util.getSizeFromZoom(map.getZoom())
+
+    if ($.type(parms.center) == "string") {
+      parms.center = Util.toLatLng(parms.center)
     }
 
     const path = []
-
     for (var i = 0, i_end = ShapeDegrees[type].length; i < i_end; i++) {
       let deg = ShapeDegrees[type][i]
       path.push(Util.getDestinationPoint(parms.center, deg, parms.size))
     }
-
     return path
   }
 
 
-  return Shapes
-})(gmap.Shapes || (gmap.Shapes = {}), gmap.Util)
+  return gmap
+})(gmap, gmap.Util)
