afa4926325b0d09f1002036a4320504c00859470
Trent Mentink <trentmentink@gmail.com>
2017-05-26 09:39:18 -0700

    add renameProperty to Util. renamed errors.js to misc.js

--- src/js/util/misc.js
+++ src/js/util/misc.js
@@ -1,5 +1,5 @@
 // ------------------------------------------------------------------------
-// GMaps: errors.js
+// GMaps: misc.js
 // ------------------------------------------------------------------------
 
 !((Util) => {
@@ -9,6 +9,21 @@
   // ----------------------------------------------------------------------
   // Public Functions
   // ----------------------------------------------------------------------
+
+  Util.renameProperty = function(parms) {
+    const obj     = parms.obj
+    const oldName = parms.oldName
+    const newName = parms.newName
+
+    if (oldName == newName) {
+      return
+    }
+
+    if (obj.hasOwnProperty(oldName)) {
+      obj[newName] = obj[oldName]
+      delete obj[oldName]
+    }
+  }
 
   Util.throwError = function(parms) {
     /* eslint-disable no-console */
