9b918bfe22144b6865f11fefb3824ac0cf02d332
Trent Mentink <trentmentink@gmail.com>
2018-02-11 20:18:03 -0800

    removed Values namespace

--- src/js/util/validation/overlays.js
+++ src/js/util/validation/overlays.js
@@ -10,16 +10,20 @@ var IsValid = ((IsValid) => {
   // Public Functions
   // ----------------------------------------------------------------------
 
+  // map     {gmap}
+  // options {object}
+  // type    {string}
   IsValid.overlayOptions = function(p) {
+    const id      = p.options.id
     const map     = p.map
     const options = p.options
     const type    = p.type
 
     // check if Id already exists
-    if (map.overlays[type].includes(options.id) === true) {
+    if (isExistingId(map, type, id)) {
       return Error.throw({
         method  : "add",
-        message : `A ${type} with an id of ${options.id} already exists`,
+        message : `A ${type} with an id of ${id} already exists`,
         obj     : options
       })
     }
@@ -29,7 +33,7 @@ var IsValid = ((IsValid) => {
     for (var i = 0, i_end = reqOptions.length; i < i_end; i++) {
       const opt = reqOptions[i].name
 
-      if (Value.isEmpty(options[opt])) {
+      if (isEmpty(options[opt])) {
         return Error.throw({
           method  : "add",
           message : `${opt} must have a value`,
@@ -41,8 +45,24 @@ var IsValid = ((IsValid) => {
     return true
   }
 
+  // type {string}
   IsValid.overlayType = function(type) {
     return Object.keys(Const.Overlays).includes(type)
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Private Functions
+  // ----------------------------------------------------------------------
+
+  function isEmpty(val) {
+    return val === ""
+        || val === null
+        || val === undefined
+  }
+
+  function isExistingId(map, type, id) {
+    return map.overlays[type].includes(id)
   }
 
 
