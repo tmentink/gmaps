17f5a56e7d4e2bf9d4edeeb695fb6efee351d1a0
Trent Mentink <trentmentink@gmail.com>
2018-02-16 17:13:37 -0800

    added deep extend function

---
+++ src/gmaps/util/extend.js
@@ -0,0 +1,30 @@
+
+var Util = ((Util) => {
+  "use strict"
+
+
+  Util.extend = function(out) {
+    out = out || {}
+
+    for (var i = 1, i_end = arguments.length; i < i_end; i++) {
+      const obj = arguments[i]
+      if (!obj) continue
+
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (typeof obj[key] === "object") {
+            out[key] = Util.extend(out[key], obj[key])
+          }
+          else {
+            out[key] = obj[key]
+          }
+        }
+      }
+    }
+
+    return out
+  }
+
+
+  return Util
+})(Util || (Util = {}))
