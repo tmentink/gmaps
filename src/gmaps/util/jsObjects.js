9df00bdee53dbb38bfc355afaddab39dd9c779a3
Trent Mentink <trentmentink@gmail.com>
2018-06-03 13:48:52 -0700

    added port of jQuery extend

--- src/gmaps/util/jsObjects.js
+++ src/gmaps/util/jsObjects.js
@@ -7,6 +7,47 @@ var Util = ((Util) => {
   // Public
   // ----------------------------------------------------------------------
 
+  Util.extend = function() {
+    let copyIsArray, clone
+    let target = arguments[0]
+
+    // eslint-disable-next-line eqeqeq
+    if (target == null || (!Is.Object(target) && !Is.Function(target))) {
+      target = {}
+    }
+
+    for (var i = 1, i_end = arguments.length; i < i_end; i++) {
+      const options = arguments[i]
+
+      // eslint-disable-next-line eqeqeq
+      if (options == null) continue
+
+      for (var name in options) {
+        const src  = target[name]
+        const copy = options[name]
+
+        if (target === copy) continue
+
+        if (copy && (isPlainObject(copy) || (copyIsArray = Is.Array(copy)))) {
+          if (copyIsArray) {
+            copyIsArray = false
+            clone = src && Is.Array(src) ? src : []
+          }
+          else {
+            clone = src && isPlainObject(src) ? src : {}
+          }
+
+          target[name] = Util.extend(clone, copy)
+        }
+        else if (copy !== undefined) {
+          target[name] = copy
+        }
+      }
+    }
+
+    return target
+  }
+
   Util.renameProperty = function({obj, oldName, newName}) {
     if (oldName === newName) return
 
@@ -14,6 +55,31 @@ var Util = ((Util) => {
       obj[newName] = obj[oldName]
       delete obj[oldName]
     }
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Private
+  // ----------------------------------------------------------------------
+
+  function isPlainObject(obj) {
+    let key
+    const hasOwn = Object.prototype.hasOwnProperty
+    const toStr  = Object.prototype.toString
+
+    if (!obj || toStr.call(obj) !== "[object Object]") return false
+
+    const hasOwnConstructor = hasOwn.call(obj, "constructor")
+    const hasIsPrototypeOf  = obj.constructor
+                           && obj.constructor.prototype
+                           && hasOwn.call(obj.constructor.prototype, "isPrototypeOf")
+
+    if (obj.constructor && !hasOwnConstructor && !hasIsPrototypeOf) return false
+
+    // eslint-disable-next-line no-empty
+    for (key in obj) {}
+
+    return typeof key === "undefined" || hasOwn.call(obj, key)
   }
 
 
