74fe2752c037e1aa894e3cd4674549d938ee648d
Trent Mentink <trentmentink@gmail.com>
2018-06-19 19:25:45 -0700

    added Is.LatLngLiteral

--- src/gmaps/util/is/googleClasses.js
+++ src/gmaps/util/is/googleClasses.js
@@ -25,6 +25,14 @@ var Is = ((Is, Google) => {
            val.hasOwnProperty("west")
   }
 
+  Is.LatLngLiteral = function(val) {
+    // eslint-disable-next-line eqeqeq
+    return val != null &&
+           Object.keys(val).length === 2 &&
+           val.hasOwnProperty("lat") &&
+           val.hasOwnProperty("lng")
+  }
+
   Is.MVCArray = function(val) {
     return val instanceof google.maps[Google.MVC_ARRAY]
   }
